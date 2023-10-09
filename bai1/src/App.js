a08ff60e726ef53e2ef3de317f0ddfaca246b8d1
NhatTruong <truongmai750@gmail.com>
2023-10-10 00:17:44 +0700

    push commit 2 element React and JSX

--- bai1/src/App.js
+++ bai1/src/App.js
@@ -2,6 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 
 //JSX: JavaScrip XML
+//ES6
+
+/**
+ * element =<div id="root">Hello World </div> :JSX
+ * element = React.CreateElement('div',{id:'root'},'Hello World')
+ * function CreateElement (ElementType, props , ...children)
+ * ElementType: 'div','p','span'
+ * props: attribute sử dụng trong HTML, className, id, src..
+ * ...children:
+ * element2= (<div>
+ * <span>Hello</span>
+ * <span>World</span>
+ * </div>)
+ *
+ * element2= React.CreateElement('div'
+ * {children:[React.CreateElement('span',null,'Hello'), ,
+ * React.CreatElement('span',null,'World')
+ * ]
+ *
+ *
+ * })
+ */
 
 function App() {
   return (
