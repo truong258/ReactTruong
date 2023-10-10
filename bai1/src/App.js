b5fffbf2d24694e3e2a0270f67aa57892ba0cd4d
NhatTruong <truongmai750@gmail.com>
2023-10-10 20:28:09 +0700

    game tictactoe phan 1

--- bai1/src/App.js
+++ bai1/src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Game from "./component/Tictactoe/Game";
 
 //JSX: JavaScrip XML
 //ES6
@@ -27,21 +27,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Game></Game>
     </div>
   );
 }
---
+++ bai1/src/component/Tictactoe/Game.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "./Board";
+import "./GameStyle.css";
+const Game = () => {
+  return (
+    <div>
+      <Board></Board>
+    </div>
+  );
+};
+
+export default Game;
