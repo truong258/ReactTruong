d086a044bdb998ba65e2b0851c8dd498f1f03206
NhatTruong <truongmai750@gmail.com>
2023-10-10 20:49:22 +0700

    game tictoe phan 2

--- bai1/src/component/Tictactoe/Board.js
+++ bai1/src/component/Tictactoe/Board.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Cell from "./Cell";
+import { calcalaterWiner } from "../../helper";
 
 const Board = () => {
+  const cells = [null, null, null, "X", "X", "X", null, null, null];
+  console.log(calcalaterWiner(cells));
+
   return (
     <div className="game-board">
       {Array(9)
