cb45919a5b7ff3e9798ba5935181981a3737d62e
NhatTruong <truongmai750@gmail.com>
2023-10-10 10:03:23 +0700

    push react

---
+++ bai1/src/components/state/Toggle.js
@@ -0,0 +1,14 @@
+//stateless functional component: component nhung khong su dung state
+//statefull functional component: component co su dung state
+
+import React from "react";
+
+function Toggle() {
+  <div className="toggle"></div>;
+}
+
+function Toggle2() {
+  return <div className="toggle"></div>;
+}
+
+export default Toggle;
